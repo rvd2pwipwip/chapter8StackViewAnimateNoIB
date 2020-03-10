1d92919365a2472fae6ee58b0a67bea5aeebaf46
Herve Desrosiers <hdesrosiers@stingray.com>
2020-03-10 14:37:33 -0400

    Initial Commit

---
+++ chapter8StackViewAnimateNoIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chapter8StackViewAnimateNoIB
+//
+//  Created by Herve Desrosiers on 2020-03-10.
+//  Copyright © 2020 Herve Desrosiers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
